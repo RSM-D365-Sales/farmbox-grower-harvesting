c05bed848e8a7d9b84dc9b2dd3d2e3fdab5390d4
Ryan Masschelin <ryan.masschelin@rsmd365s.com>
2026-04-10 11:06:56 -0700

    vite config changes

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename="/grower-harvesting">
+      <BrowserRouter basename="/farmbox-grower-harvesting">
         <AuthProvider>
           <App />
         </AuthProvider>
